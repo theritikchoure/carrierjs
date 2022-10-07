675e08e0da225072df7c7f22b5918d324031751b
Ritik Chourasiya <ritvikchoure65@gmail.com>
2022-10-07 16:21:13 +0530

    removed: script loader function

--- carrier.js
+++ carrier.js
@@ -9,12 +9,6 @@
     "use strict";
     let cache = JSON.parse(localStorage.getItem('carrierCache'));
 
-    let appendScript = function(res) {
-        var c = b.head || b.getElementsByTagName("head")[0];
-        var d = b.createElement("script");
-        d.defer = !0, d.text = res.response, c.appendChild(d)
-    };
-
     let cacheHasProperty = (url) => {
         return cache && cache.hasOwnProperty(url)
     }
@@ -24,7 +18,7 @@
     }
 
     a.carrier = {
-        get: function(url) {
+        get: function(url, refresh = false) {
             let promise = new Promise((resolve, reject) =>{
 
                 if(isEmpty(url)) {
@@ -32,38 +26,55 @@
                     return;
                 }
 
-                let req = new XMLHttpRequest();
-                req.responseType = 'json';
-                req.open("GET", url);
+                if(!refresh && cacheHasProperty(url)) {
+                    const res = cache[url];
+                    resolve(res)
+                } else {
 
-                req.send();
+                    let req = new XMLHttpRequest();
+                    req.responseType = 'json';
+                    req.open("GET", url);
 
-                req.onload = () => {
+                    req.send();
 
-                    // Response Object
-                    const res = {
-                        config: '',
-                        response: eval(req.response),
-                        headers: req.getAllResponseHeaders(),
-                        request: req.readyState,
-                        url: req.responseURL,
-                        status: req.status,
-                        type: 'json'
+                    req.onload = () => {
+
+                        // Response Object
+                        const res = {
+                            config: '',
+                            response: eval(req.response),
+                            headers: req.getAllResponseHeaders(),
+                            request: req.readyState,
+                            url: req.responseURL,
+                            status: req.status,
+                            type: 'json'
+                        }
+
+                        if(req.status === 200) {
+                            // Cache is null or blank
+                            if(cache === null) {
+                                cache = {};
+                                cache[url] = res
+                                localStorage.setItem('carrierCache', JSON.stringify(cache));
+                            }
+
+                            cache[url] = res        
+                            localStorage.setItem('carrierCache', JSON.stringify(cache));
+
+                            resolve(res)
+                        }
+
+                        if(req.status === 404) {
+                            reject(res)
+                        }
                     }
 
-                    if(req.status === 200) {
-                        resolve(res)
+                    req.onreadystatechange = () => {
+                        if(req.readyState === 4 && req.status === 404) {
+                            // TODO
+                        }
                     }
 
-                    if(req.status === 404) {
-                        reject(res)
-                    }
-                }
-
-                req.onreadystatechange = () => {
-                    if(req.readyState === 4 && req.status === 404) {
-                        // TODO
-                    }
                 }
             });
 
@@ -106,61 +117,6 @@
                 }
             });
 
-            return promise;
-        },
-        script: function(url) {
-            let promise = new Promise((resolve, reject) =>{
-
-                if(isEmpty(url)) {
-                    console.error(new Error('Script source is required to make request'))
-                    return;
-                }
-        
-                if(cacheHasProperty(url)) {
-                    const res = cache[url];
-                    if(res.status === 200) {
-                        appendScript(res);
-                        resolve(res.response)
-                    }
-                    resolve(res)
-                } else {
-
-                    const req = new XMLHttpRequest();
-                    req.responseType = 'text';
-
-                    req.open("GET", url);
-                    req.onload = () => {
-                        const res = {
-                            config: '',
-                            response: req.response,
-                            headers: req.getAllResponseHeaders(),
-                            request: req.readyState,
-                            url: req.responseURL,
-                            status: req.status,
-                            type: 'script'
-                        }
-
-                        if (req.status === 200) {
-
-                            // Cache is null or blank
-                            if(cache === null) {
-                                cache = {};
-                                cache[url] = res
-                                localStorage.setItem('carrierCache', JSON.stringify(cache));
-                            }
-
-                            cache[url] = res        
-                            localStorage.setItem('carrierCache', JSON.stringify(cache));
-
-                            appendScript(res);
-                            resolve(res.response)
-                        }
-                    }
-                    req.send();
-                }
-        
-            })
-        
             return promise;
         },
     }
