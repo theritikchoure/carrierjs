00c21b7dc91e44731be7ee9d9e54eadfec4cb971
Ritik Chourasiya <ritvikchoure65@gmail.com>
2022-10-08 16:33:57 +0530

    added indexedDB for caching

--- carrier.js
+++ carrier.js
@@ -7,28 +7,103 @@
 
 ! function(a, b) {
     "use strict";
-    let cache = JSON.parse(localStorage.getItem('carrierCache'));
-
-    let cacheHasProperty = (url) => {
-        return cache && cache.hasOwnProperty(url)
-    }
+    let db = null;
 
     let isEmpty = (a) => {
-        if(a === undefined || a === null || a.length === 0) return true
+        if(a === undefined || a === null || a.length === 0) return true;
     }
+
+    // create indexedDB instance
+    let createIndexedDB = () => {
+        let promise = new Promise((resolve, reject) => {
+            const request = indexedDB.open('carrierjs');
+
+            //on upgrade needed
+            request.onupgradeneeded = e => {
+                db = e.target.result;
+                const responsesDB = db.createObjectStore("responses", {keyPath: "url"})
+            }
+    
+            //on success 
+            request.onsuccess = e => {
+                db = e.target.result;
+                // db.name = "responses";
+                resolve();
+            }
+    
+            //on error
+            request.onerror = e => {
+                console.log("onError", e);
+            }
+        });
+
+        return promise;
+    }
+
+    // check db has response data, if yes then return it;
+    let dbHasResponse = (url = null) => {
+        let promise = new Promise(function(resolve, reject) {
+            if(!url) {
+                console.log(new Error("Invalid url"));
+                return;
+            };
+
+            const tx = db.transaction("responses","readonly")
+            const requestStore = tx.objectStore("responses")
+            const request = requestStore.get(url);
+            let res = undefined;
+            request.onsuccess = e => {
+
+                res = e.target.result;
+                resolve(res);
+            };
+        });
+
+        return promise;
+    }
+
+    let addResponseToDB = (response) => {
+        let promise = new Promise(function(resolve, reject) {
+
+            const tx = db.transaction("responses", "readwrite")
+            tx.onerror = e => console.log(e.target);
+            const responseStore = tx.objectStore("responses");
+            responseStore.add(response);
+            resolve(response);
+        });
+        return promise;
+    };
+
+    let updateResponseToDB = (object) => {
+        let promise = new Promise((resolve, reject) => {
+            const tx = db.transaction("responses", "readwrite")
+            tx.onerror = e => console.log(e.target);
+            const responseStore = tx.objectStore("responses");
+            responseStore.put(object);
+            resolve(object);
+        });
+
+        return promise;
+    };
+    
+    (async () => {
+        await createIndexedDB();
+    })();
 
     a.carrier = {
         get: function(url, refresh = false) {
-            let promise = new Promise((resolve, reject) =>{
+            let promise = new Promise(async (resolve, reject) =>{
 
                 if(isEmpty(url)) {
                     console.error(new Error('Url is required to make request'))
                     return;
                 }
 
-                if(!refresh && cacheHasProperty(url)) {
-                    const res = cache[url];
-                    resolve(res)
+                await createIndexedDB();
+                let response = await dbHasResponse(url);
+
+                if(!refresh && response) {
+                    resolve(response);
                 } else {
 
                     let req = new XMLHttpRequest();
@@ -37,35 +112,34 @@
 
                     req.send();
 
-                    req.onload = () => {
+                    req.onload = async () => {
 
                         // Response Object
                         const res = {
                             config: '',
-                            response: eval(req.response),
+                            response: req.response,
                             headers: req.getAllResponseHeaders(),
                             request: req.readyState,
                             url: req.responseURL,
                             status: req.status,
                             type: 'json'
-                        }
+                        };
 
                         if(req.status === 200) {
-                            // Cache is null or blank
-                            if(cache === null) {
-                                cache = {};
-                                cache[url] = res
-                                localStorage.setItem('carrierCache', JSON.stringify(cache));
+
+                            if(response) {
+                                await updateResponseToDB(res);
+                            }
+                            
+                            if(!response) {
+                                await addResponseToDB(res);
                             }
 
-                            cache[url] = res        
-                            localStorage.setItem('carrierCache', JSON.stringify(cache));
-
-                            resolve(res)
+                            resolve(res);
                         }
 
                         if(req.status === 404) {
-                            reject(res)
+                            reject(new Error('Url not Found'));
                         }
                     }
 
@@ -93,20 +167,20 @@
                     // Response Object
                     const res = {
                         config: '',
-                        response: eval(req.response),
+                        response: req.response,
                         headers: req.getAllResponseHeaders(),
                         request: req.readyState,
                         url: req.responseURL,
                         status: req.status,
                         type: 'json'
-                    }
+                    };
 
                     if(req.status === 200 || req.status === 201) {
-                        resolve(res)
+                        resolve(res);
                     }
 
                     if(req.status === 404) {
-                        reject(res)
+                        reject(res);
                     }
                 }
 
