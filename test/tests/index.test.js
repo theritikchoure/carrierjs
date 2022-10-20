d74047f6bee85928e6ace45204b1587455af1af1
Ritik Chourasiya <ritvikchoure65@gmail.com>
2022-10-20 15:11:09 +0530

    added some basic tests in index.test.js

--- test/tests/index.test.js
+++ test/tests/index.test.js
@@ -1,4 +1,5 @@
 mocha.setup("bdd");
+mocha.checkLeaks();
 var expect = chai.expect;
 let assert = chai.assert;
 
@@ -15,5 +16,20 @@ describe("Testing Get Method", function async () {
   it("result.response should be an array", async function () {
     let result = await getMethod();
     assert.isArray(result.response);
+  });
+  
+  it("result.headers should be an object", async function () {
+    let result = await getMethod();
+    assert.isObject(result.headers);
+  });
+
+  it("response status code should be 200 ", async function () {
+    let result = await getMethod();
+    assert.strictEqual(result.status, 200);
+  });
+  
+  it("response length should be 6", async function () {
+    let result = await getMethod();
+    assert.lengthOf(result, 6);
   });
 });
