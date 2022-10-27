8edfa79f0d7cde7194432220b9e163d3eaa51359
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2022-10-27 13:25:09 +0530

    feat: add scroll to top feature button feature (#35)
    * feat: add scroll to top feature button feature
    
    * refactor: scroll to top button
    
    Co-authored-by: Ritik Chourasiya <ritvikchoure65@gmail.com>

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -92,3 +92,31 @@ var spy = new Gumshoe('#docs-nav a', {
 /*  Ref: https://github.com/andreknieriem/simplelightbox */
 
 var lightbox = new SimpleLightbox('.simplelightbox-gallery a', {/* options */});
+
+// Top to bottom button functions (Start)
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+	document.body.scrollTop > 0 ||
+	document.documentElement.scrollTop > 0
+  ) {
+	mybutton.style.display = "block";
+  } else {
+	mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// Top to bottom button functions (End)
